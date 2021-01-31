e3e72a1ced14ba3b35d1c815f2a764f35b68f901
Shashwat Navinkumar <000790494@mohawkcollege.ca>
2021-01-31 14:03:28 -0500

    Initital Commit

---
+++ Lab5a/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Lab5a
+{
+    partial class bucketFill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(bucketFill));
+            this.color_btn = new System.Windows.Forms.Button();
+            this.close_btn = new System.Windows.Forms.Button();
+            this.waterflow_trackbar = new System.Windows.Forms.TrackBar();
+            this.tap = new System.Windows.Forms.PictureBox();
+            this.bucket = new System.Windows.Forms.Panel();
+            this.waterColor = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.waterflow_trackbar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // color_btn
+            // 
+            this.color_btn.Location = new System.Drawing.Point(34, 12);
+            this.color_btn.Name = "color_btn";
+            this.color_btn.Size = new System.Drawing.Size(75, 31);
+            this.color_btn.TabIndex = 0;
+            this.color_btn.Text = "Color";
+            this.color_btn.UseVisualStyleBackColor = true;
+            this.color_btn.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // close_btn
+            // 
+            this.close_btn.Location = new System.Drawing.Point(150, 12);
+            this.close_btn.Name = "close_btn";
+            this.close_btn.Size = new System.Drawing.Size(75, 31);
+            this.close_btn.TabIndex = 1;
+            this.close_btn.Text = "Close";
+            this.close_btn.UseVisualStyleBackColor = true;
+            this.close_btn.Click += new System.EventHandler(this.Close_btn_Click);
+            // 
+            // waterflow_trackbar
+            // 
+            this.waterflow_trackbar.Location = new System.Drawing.Point(34, 89);
+            this.waterflow_trackbar.Name = "waterflow_trackbar";
+            this.waterflow_trackbar.Size = new System.Drawing.Size(123, 56);
+            this.waterflow_trackbar.TabIndex = 2;
+            // 
+            // tap
+            // 
+            this.tap.Image = ((System.Drawing.Image)(resources.GetObject("tap.Image")));
+            this.tap.Location = new System.Drawing.Point(34, 168);
+            this.tap.Name = "tap";
+            this.tap.Size = new System.Drawing.Size(123, 83);
+            this.tap.TabIndex = 3;
+            this.tap.TabStop = false;
+            // 
+            // bucket
+            // 
+            this.bucket.ForeColor = System.Drawing.Color.Transparent;
+            this.bucket.Location = new System.Drawing.Point(98, 236);
+            this.bucket.Name = "bucket";
+            this.bucket.Size = new System.Drawing.Size(280, 258);
+            this.bucket.TabIndex = 4;
+            this.bucket.Paint += new System.Windows.Forms.PaintEventHandler(this.Bucket_Paint);
+            // 
+            // bucketFill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(430, 522);
+            this.Controls.Add(this.bucket);
+            this.Controls.Add(this.tap);
+            this.Controls.Add(this.waterflow_trackbar);
+            this.Controls.Add(this.close_btn);
+            this.Controls.Add(this.color_btn);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "bucketFill";
+            this.Text = "A drop in the bucket";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.waterflow_trackbar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tap)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button color_btn;
+        private System.Windows.Forms.Button close_btn;
+        private System.Windows.Forms.TrackBar waterflow_trackbar;
+        private System.Windows.Forms.PictureBox tap;
+        private System.Windows.Forms.Panel bucket;
+        private System.Windows.Forms.ColorDialog waterColor;
+    }
+}
+
---
+++ Lab5a/Form1.cs
@@ -0,0 +1,164 @@
+﻿/* Name : Shashwat Kumar
+ * Student No.: 000790494
+ * I certify that this is my original work.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5a
+{
+    public partial class bucketFill : Form
+    {
+        Timer bucketFillTimer;
+        Color color = Color.LightBlue;
+        float completionPercentage = 100;
+        bool bucketFull = false;
+        /// <summary>
+        /// Initializes component and creates a new timer
+        /// </summary>
+        public bucketFill()
+        {
+            InitializeComponent();
+            bucketFillTimer = new Timer();
+            bucketFillTimer.Interval = 1000 / 1000;
+            bucketFillTimer.Tick += BucketFillTimer_Tick;
+            
+        }
+
+        /// <summary>
+        /// This calls update and draw method if the value of the trackbar > 0  
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BucketFillTimer_Tick(object sender, EventArgs e)
+        {
+            if (waterflow_trackbar.Value != 0)
+            {
+                Update();
+                Draw();
+
+            }
+            if (waterflow_trackbar.Value == 0 && completionPercentage!=100)
+            {
+                using (Graphics g = bucket.CreateGraphics())
+                {
+                    Brush b = new SolidBrush(Color.Black);
+                    float y = (completionPercentage / 100) * (bucket.Height);
+                    g.FillRectangle(b, 21, 0, 15, y);
+                    
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// This creates a bucket and starts the timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            using (Graphics g = this.CreateGraphics())
+            {
+                Pen pen = new Pen(Color.White);
+                
+                g.DrawLine(pen, bucket.Location.X-1, bucket.Location.Y+90, bucket.Location.X-1, bucket.Location.Y+bucket.Height);
+                g.DrawLine(pen, bucket.Location.X-1, bucket.Location.Y + bucket.Height, bucket.Location.X+bucket.Width, bucket.Location.Y + bucket.Height);
+                g.DrawLine(pen, bucket.Location.X + bucket.Width, bucket.Location.Y + bucket.Height, bucket.Location.X + bucket.Width, bucket.Location.Y +90);
+            }
+            bucketFillTimer.Start();
+            
+        }
+
+        /// <summary>
+        /// This method draws water flow and increases the level water in the bucket 
+        /// Manages the speed of the water flow as the value of the trackbar changes
+        /// </summary>
+        public void Update()
+        {
+            
+
+            if (completionPercentage <= 48)
+            {
+                waterflow_trackbar.Value = 0;
+                
+                bucketFull = true;
+
+                using (Graphics g = bucket.CreateGraphics())
+                {
+                    Brush b = new SolidBrush(Color.Black);
+                    float y = (completionPercentage / 100) * (bucket.Height);
+                    g.FillRectangle(b, 21, 0, 15, y);
+                }
+                completionPercentage = 100;
+            }
+            if (bucketFull && completionPercentage < 100)
+            {
+                using (Graphics g = bucket.CreateGraphics())
+                {
+                    g.Clear(Color.Black);
+                    bucketFull = false;
+                }
+            }
+            float percentageDelta = (float)waterflow_trackbar.Value/25;
+            completionPercentage -= percentageDelta;
+        }
+
+        /// <summary>
+        /// This method draws the water level and flowing water
+        /// </summary>
+        public void Draw()
+        {
+            using (Graphics g = bucket.CreateGraphics())
+            { 
+                Brush b = new SolidBrush(color);
+                float x = 0;
+                float width = bucket.Width;
+                float height = 1;
+                float y = (completionPercentage / 100) * (bucket.Height);
+                
+                g.FillRectangle(b, x, y, width, height);
+                if (completionPercentage < 100)
+                {                 
+                    g.FillRectangle(b, 21, 0, 15,y);                   
+                }
+            }
+        }
+
+        /// <summary>
+        /// Changes the color of the water according to what user selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(waterColor.ShowDialog() == DialogResult.OK)
+            {
+                color = waterColor.Color;
+            }
+        }
+
+        /// <summary>
+        /// Closes the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Close_btn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Bucket_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
